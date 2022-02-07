232cf6fa8227597f18a5c97e156cae373d982c00
Matouš Zatloukal <matouszatloukal@seznam.cz>
2022-02-07 16:05:32 +0100

    hotovo

--- main.py
+++ main.py
@@ -12,18 +12,31 @@ class Application(tk.Tk):
 
     def __init__(self):
         super().__init__(className=self.name)
+        self.lbl = tk.Label(self, text="matematikovací věda")
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.frame = tk.Frame(self, highlightbackground="#000000")
-        self.frame.pack(padx=50, pady=25)
-        self.lbl = tk.Label(self.frame, text="Ahoj kamosu")
-        self.lbl.pack()
-        self.entry = tk.Entry(self.frame)
-        self.entry.pack()
-        self.btn2 = tk.Button(self, text="Pokracovat", command=self.generovani)
-        self.btn2.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
+        self.lbl.grid(row=0)
+        self.lblGr = tk.Label(self, text="")
+        self.lblGr.grid(row=3, column=2)
+        self.generovani()
+        self.entry = tk.Entry(self, width=5)
+        self.entry.grid(row=2, column=3)
+        self.bind('<Return>', self.kontrola)
+        self.but = tk.Button(self, text="Konec", command=self.quit)
+        self.but.grid(row=3, column=3)
+
+    def kontrola(self, event):
+        try:
+            if int(self.entry.get()) == self.vysledek:
+                self.lblGr.config(text="SPRÁVNĚ", fg="green")
+
+            else:
+                self.lblGr.config(text="ŠPATNĚ",fg="red")
+
+            self.generovani()
+            self.entry.delete(0, tk.END)
+        except ValueError:
+            return
 
 
     def plus(self):
@@ -32,11 +45,15 @@ class Application(tk.Tk):
         self.vysledek = self.cisloA + self.cisloB
         self.lbl.config(text=f'{self.cisloA} + {self.cisloB} = ')
 
+        return self.lbl.config(text=f'{self.cisloA} + {self.cisloB} = ')
+
     def minus(self):
         self.cisloA = random.randint(1, 100)
         self.cisloB = random.randint(1, self.cisloA)
         self.vysledek = self.cisloA - self.cisloB
         self.lbl.config(text=f'{self.cisloA} - {self.cisloB} = ')
+
+        return self.lbl.config(text=f'{self.cisloA} - {self.cisloB} = ')
 
     def krat(self):
         self.cisloA = random.randint(1, 9)
@@ -44,11 +61,15 @@ class Application(tk.Tk):
         self.vysledek = self.cisloA * self.cisloB
         self.lbl.config(text=f'{self.cisloA} * {self.cisloB} = ')
 
+        return self.lbl.config(text=f'{self.cisloA} * {self.cisloB} = ')
+
     def deleno(self):
         self.vysledek = random.randint(1, 9)
         self.cisloB = random.randint(1, 9)
         self.cisloA = self.vysledek * self.cisloB
         self.lbl.config(text=f'{self.cisloA} / {self.cisloB} = ')
+
+        return self.lbl.config(text=f'{self.cisloA} / {self.cisloB} = ')
 
     def generovani(self):
         priklad = random.choice([self.plus, self.minus, self.krat, self.deleno])
